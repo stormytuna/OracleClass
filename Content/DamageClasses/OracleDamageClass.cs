a8870223b3b7f682442da7fde25420d56b459ca6
stormytuna <stormytuna@outlook.com>
2022-11-16 17:21:14 +0000

    Create OracleDamageClass.cs

---
+++ Content/DamageClasses/OracleDamageClass.cs
@@ -0,0 +1,19 @@
+﻿using Terraria.ModLoader;
+
+namespace OracleClass.Content.DamageClasses {
+    // For a more indepth explanation of all the stuff here see example damage class
+    // https://github.com/tModLoader/tModLoader/blob/daa239205ab69743d44f7bb64c9cd3e080024a51/ExampleMod/Content/DamageClasses/ExampleDamageClass.cs
+    public class OracleDamageClass : DamageClass {
+        public override StatInheritanceData GetModifierInheritance(DamageClass damageClass) {
+            if (damageClass == DamageClass.Generic) {
+                return StatInheritanceData.Full;
+            }
+
+            return new StatInheritanceData(1f, 1f, 1f, 1f, 1f);
+        }
+
+        public override bool UseStandardCritCalcs => true;
+
+        protected override string DisplayNameInternal => "oracle damage";
+    }
+}
