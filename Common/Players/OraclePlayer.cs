5389105acc42b37e7389b02ed6f9cd42a477c13b
stormytuna <stormytuna@outlook.com>
2022-11-16 17:21:09 +0000

    Create OraclePlayer.cs

---
+++ Common/Players/OraclePlayer.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ModLoader;
+
+namespace OracleClass.Common.Players {
+    public class OraclePlayer : ModPlayer {
+        /// <summary>The multiplier to base soul capacity from the players accessories and armor</summary>
+        public float SoulCapacityMultiplier { get; set; }
+
+        public override void ResetEffects() {
+            SoulCapacityMultiplier = 1f;
+        }
+    }
+}
