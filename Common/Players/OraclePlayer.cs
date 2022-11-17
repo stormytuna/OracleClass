a7b7585f7c8b9737c12ee229e4e3e05f79be8df5
stormytuna <stormytuna@outlook.com>
2022-11-17 10:26:55 +0000

    Add a reduce soul on all weapons function

--- Common/Players/OraclePlayer.cs
+++ Common/Players/OraclePlayer.cs
@@ -1,4 +1,5 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ModLoader;
 
 namespace OracleClass.Common.Players {
     public class OraclePlayer : ModPlayer {
@@ -7,6 +8,23 @@ namespace OracleClass.Common.Players {
 
         public override void ResetEffects() {
             SoulCapacityMultiplier = 1f;
+        }
+
+        /// <summary>
+        /// This goes through your inventory and reduces the soul capacity of each weapon with that type. Call this whenever you reduce an items soul capacity<para />
+        /// Weapons will exhaust from this if it reduces them to 0
+        /// </summary>
+        public void ReduceSoulOnAllWeapons(Item itemToIgnore) {
+            foreach (var item in Player.inventory) {
+                if (item.type == itemToIgnore.type && item != itemToIgnore) {
+                    var oracleWeapon = item.GetOracleWeapon();
+                    oracleWeapon.CurSoulCapacity--;
+                    oracleWeapon.SoulRecoveryCooldown = 5 * 60;
+                    if (oracleWeapon.CurSoulCapacity <= 0) {
+                        oracleWeapon.Exhausted = true;
+                    }
+                }
+            }
         }
     }
 }
