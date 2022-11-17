139a5376c27f34956cf6a53da1a17864ee722ae6
stormytuna <stormytuna@outlook.com>
2022-11-17 10:27:13 +0000

    Add mod call for colored damage types

--- OracleClass.cs
+++ OracleClass.cs
@@ -1,6 +1,17 @@
+using Microsoft.Xna.Framework;
+using OracleClass.Content.DamageClasses;
 using Terraria.ModLoader;
 
 namespace OracleClass {
     public class OracleClass : Mod {
+        public override void PostSetupContent() {
+            // Mod calls
+            // This adds a colour to our damage type
+            if (ModLoader.TryGetMod("ColoredDamageTypes", out Mod coloredDamageTypes)) {
+                coloredDamageTypes.Call("AddDamageType", ModContent.GetInstance<OracleDamageClass>(), new Color(162, 32, 88), new Color(162, 32, 88), new Color(112, 22, 60));
+            }
+
+            base.PostSetupContent();
+        }
     }
 }
