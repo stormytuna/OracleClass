f570e8e8147729e014cb78e9d9b4a31521fa5522
stormytuna <stormytuna@outlook.com>
2022-12-08 15:36:33 +0000

    Create Helpers.cs

---
+++ Helpers.cs
@@ -0,0 +1,34 @@
+﻿using Terraria.ID;
+
+namespace OracleClass {
+    public static class Helpers {
+        public static int[] universalPrefixes = new int[] {
+            // Universals
+            PrefixID.Keen,
+            PrefixID.Superior,
+            PrefixID.Forceful,
+            PrefixID.Broken,
+            PrefixID.Damaged,
+            PrefixID.Shoddy,
+            PrefixID.Hurtful,
+            PrefixID.Strong,
+            PrefixID.Unpleasant,
+            PrefixID.Weak,
+            PrefixID.Ruthless,
+            PrefixID.Godly,
+            PrefixID.Demonic,
+            PrefixID.Zealous,
+            // Common
+            PrefixID.Quick,
+            PrefixID.Deadly,
+            PrefixID.Agile,
+            PrefixID.Nimble,
+            PrefixID.Murderous,
+            PrefixID.Slow,
+            PrefixID.Sluggish,
+            PrefixID.Lazy,
+            PrefixID.Annoying,
+            PrefixID.Nasty
+        };
+    }
+}
